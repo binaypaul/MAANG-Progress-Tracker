7fcceb306e08665c499cbe0bea8209b15ebd7a0d
Binay Paul <binay.paul.1993@gmail.com>
2024-04-23 09:20:26 +0530

    Longest substring without repeating char solved

--- src/main/java/org/maangprep/A2_LongestSubstringWithoutRepeatingCharacters_Medium.java
+++ src/main/java/org/maangprep/A2_LongestSubstringWithoutRepeatingCharacters_Medium.java
@@ -1,12 +1,33 @@
 package org.maangprep;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 public class A2_LongestSubstringWithoutRepeatingCharacters_Medium {
     public int lengthOfLongestSubstring(String s) {
+        int startIndex = 0;
+        String maxSub = "";
 
-        return 0;
+        Map<String, Integer> map = new HashMap<>();
+        String[] sar = s.toLowerCase().split("");
+
+        for(int i = 0; i < sar.length; i++) {
+            Integer indexOfRepChar = map.get(sar[i]);
+
+            if (indexOfRepChar != null && indexOfRepChar >= startIndex) {
+                String sub = s.substring(startIndex, i);
+                if(sub.length() > maxSub.length()) maxSub = sub;
+                startIndex = indexOfRepChar + 1;
+
+            }
+            map.put(sar[i], i);
+            indexOfRepChar = null;
+        }
+        return maxSub.length();
     }
 
     public static void main(String[] args) {
-        new A2_LongestSubstringWithoutRepeatingCharacters_Medium().lengthOfLongestSubstring("abcabcbb");
+        System.out.println(new A2_LongestSubstringWithoutRepeatingCharacters_Medium().lengthOfLongestSubstring("a"));
     }
 }
